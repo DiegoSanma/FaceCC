6d06341bfd13f8ba914e379568814d8e3a2bb668
DiegoSanma <diego.sanmartin.a@ug.uchile.cl>
2025-10-13 18:39:17 -0300

    feat: Comienza implementación con facenet

--- facecc/app.py
+++ facecc/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import base64 
 import json
+import pickle,os
+import numpy as np
+import cv2
+from deepface import DeepFace
 
 app = Flask(__name__)
 
@@ -14,12 +18,24 @@ def process_image():
     image_data = data.get("image", "Sin nombre")
     format, imgstr = image_data.split(';base64,') 
     ext = format.split('/')[-1] 
-    #image_file = ContentFile(base64.b64decode(imgstr), name='captured_image.' + ext)
-    
+    image_file = base64.b64decode(imgstr)
+    nparr = np.frombuffer(image_file, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    prediccion = DeepFace.find(img_path=img, db_path = "CaraTui",
+                               model_name = 'Facenet',
+                               silent = True,
+                               enforce_detection = False)
+    reconocimiento = "Desconocido"
+    if prediccion and not prediccion[0].empty:
+        df = prediccion[0]
+        predicted_path = df['identity'].iloc[0]
+        predicted_name = os.path.basename(predicted_path).split('.')[0]
+        distance = df['distance'].iloc[0]
+        if distance < 0.6:
+            reconocimiento = predicted_name
     # Aquí la idea sería realiazar el procesamiento de la imagen y ver si coincide con alguna cara
-    reconomiento = "Diego"
     #Luego la entrego al fetch del javascript para que que se vea el resultado
-    return jsonify({'name': reconomiento})
+    return jsonify({'name': reconocimiento})
     
 
 if __name__ == '__main__':
